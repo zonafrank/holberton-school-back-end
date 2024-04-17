23e7defa07d7b9e57e6ddfe38a3afdd77a787530
Echezona Oji <echyth@yahoo.com>
2024-04-17 23:12:30 +1000

    feat(task 1): export to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""for a given employee ID, returns information about
+    his/her TODO list progress
+"""
+
+import sys
+import requests
+import csv
+
+BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+
+def get_user_todos(user_id):
+    """for a given employee ID, returns information about
+    his/her TODO list progress
+    """
+    res = requests.get(f"{BASE_URL}/users/{user_id}")
+    user = res.json()
+    res = requests.get(f"{BASE_URL}/users/{user_id}/todos")
+    todos = res.json()
+    return {"user": user, "todos": todos}
+
+def write_to_file(obj):
+    user = obj["user"]
+    todos = obj["todos"]
+    
+    with open(f"{user['id']}.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        for todo in todos:
+            writer.writerow([user["id"], user["name"], todo["completed"], todo["title"]])
+
+
+if __name__ == "__main__":
+    _, id = sys.argv
+    obj = get_user_todos(id)
+    write_to_file(obj)
