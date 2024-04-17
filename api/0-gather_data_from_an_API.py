656364fa18d9dacab445740d8fd1b7db0513ee00
Echezona Oji <echyth@yahoo.com>
2024-04-17 22:54:34 +1000

    feat(task 0): gather data from an api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""for a given employee ID, returns information about
+    his/her TODO list progress
+"""
+
+import sys
+import requests
+
+BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+
+def get_user_todos(user_id):
+    """for a given employee ID, returns information about
+    his/her TODO list progress
+    """
+    res = requests.get(f"{BASE_URL}/users/{user_id}")
+    user = res.json()
+    res = requests.get(f"{BASE_URL}/users/{user_id}/todos")
+    todos = res.json()
+    completed_todos = [todo for todo in todos if todo["completed"]]
+    stats = f"{len(completed_todos)}/{len(todos)}"
+    print(f"Employee {user['name']} is done with tasks ({stats}):")
+    for todo in completed_todos:
+        print(f"\t {todo['title']}")
+
+
+if __name__ == "__main__":
+    _, id = sys.argv
+    get_user_todos(id)
